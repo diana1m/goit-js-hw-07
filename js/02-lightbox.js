79abe5ebf4f68e1f708d9db6952f7a529edb3885
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-17 23:55:02 +0200

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description})=>
+    `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+    </a> `
+).join("")
+
+galleryBox.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+
